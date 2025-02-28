d9edb5eef06741fe86ff836561c223023d4c0516
James Olinya <olinyajames@gmail.com>
2025-02-28 22:29:14 +0000

    create recent transactions component

--- src/components/dashboard/RecentTransactions.tsx
+++ src/components/dashboard/RecentTransactions.tsx
@@ -1,5 +1,37 @@
-const RecentTransactions = () => {
-  return (<></>);
+import { Transaction } from "../../types/common";
+import Avatar from "../Avatar/Avatar";
+import SectionTitle from "./SectionTitle";
+
+interface Props {
+  className?: string;
+  transactions: Array<Transaction>;
+}
+
+const RecentTransactions = ({ className = "", transactions }: Props) => {
+  return (
+    <div className={`${className} flex flex-col`}>
+      <SectionTitle title="Recent Transactions" />
+      <ul className="bg-white rounded-[25px] px-6 py-5 space-y-3 flex-1 overflow-y-auto">
+        {transactions.map((transaction, i) => (
+          <li key={`transaction-${i}`} className="flex space-x-5 items-center">
+            <Avatar size="lg" />
+            <div className="flex-1">
+              <div>{transaction.narration}</div>
+              <small className="text-[#718EBF]">{transaction.date}</small>
+            </div>
+            <div
+              className={
+                transaction.type === "debit" ? "text-red-500" : "text-green-500"
+              }
+            >
+              {transaction.type === "debit" ? "-" : "+"}
+              {transaction.amount}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default RecentTransactions;
