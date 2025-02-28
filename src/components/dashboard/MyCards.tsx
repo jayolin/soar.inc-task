b303a34fcecde3bb75b62b2e42fc1bc97d8445b3
James Olinya <olinyajames@gmail.com>
2025-02-28 22:28:42 +0000

    create my cards component

--- src/components/dashboard/MyCards.tsx
+++ src/components/dashboard/MyCards.tsx
@@ -1,5 +1,33 @@
-const MyCards = () => {
-  return (<></>);
+import { Link } from "react-router-dom";
+import Card from "../Card/Card";
+import SectionTitle from "./SectionTitle";
+import { useApp } from "../../context/AppContext";
+
+interface Props {
+  className?: string;
+}
+
+const MyCards = ({ className = "" }: Props) => {
+  const { userData } = useApp();
+
+  if (!userData) {
+    return <></>;
+  }
+
+  return (
+    <div className={`${className}`}>
+      <SectionTitle title="My Cards">
+        <Link className="hover:underline active:text-blue-500" to={"/"}>See All</Link>
+      </SectionTitle>
+      <div className="overflow-x-auto">
+        <div className="w-max gap-4 max-h-[210px] columns-[300px] lg:max-h-[235px] lg:columns-[350px]">
+          {userData.cards.map((card, i) => (
+            <Card {...card} key={`card-${i}`} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MyCards;
