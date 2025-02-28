05aa757e28feba42e0ef7a03698b2598580d08f9
James Olinya <olinyajames@gmail.com>
2025-02-28 22:33:10 +0000

    create settings page

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,21 +1,62 @@
 import { useState } from "react";
 import EditProfile from "../components/settings/EditProfile";
+import TabHeader from "../components/TabHeader/TabHeader";
 
 const Settings = () => {
-  const [activeTab, setActiveTab] = useState('Edit Profile');
+  const [activeTab, setActiveTab] = useState("edit-profile");
 
   return (
-    <div className="p-4">
+    <div className="p-6 lg:m-8 m-6 rounded-[25px] bg-white">
       <div className="tabs">
-        {['Edit Profile', 'Preferences', 'Security'].map((tab) => (
-          <button key={tab} onClick={() => setActiveTab(tab)}>
-            {tab}
-          </button>
-        ))}
+        <TabHeader
+          tabs={[
+            {
+              id: "edit-profile",
+              name: "Edit Profile",
+            },
+            {
+              id: "preferences",
+              name: "Preferences",
+            },
+            {
+              id: "security",
+              name: "Security",
+            },
+          ]}
+          activeTab={activeTab}
+          onRequestChangeTab={setActiveTab}
+        />
       </div>
-      {activeTab === 'Edit Profile' && <EditProfile />}
-      {activeTab === 'Preferences' && <></>}
-      {activeTab === 'Security' && <></>}
+
+      <div
+        className={`transition-opacity duration-500 ${
+          activeTab === "edit-profile"
+            ? "opacity-100 visible"
+            : "opacity-0 invisible"
+        }`}
+      >
+        <EditProfile />
+      </div>
+
+      <div
+        className={`transition-opacity duration-500 ${
+          activeTab === "preferences"
+            ? "opacity-100 visible"
+            : "opacity-0 invisible"
+        }`}
+      >
+        <></>
+      </div>
+
+      <div
+        className={`transition-opacity duration-500 ${
+          activeTab === "security"
+            ? "opacity-100 visible"
+            : "opacity-0 invisible"
+        }`}
+      >
+        <></>
+      </div>
     </div>
   );
 };
