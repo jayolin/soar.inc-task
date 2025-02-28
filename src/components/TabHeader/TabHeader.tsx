739e0284af6ef88e47559289d3820631a9d93726
James Olinya <olinyajames@gmail.com>
2025-02-28 22:21:46 +0000

    create tab header component

---
+++ src/components/TabHeader/TabHeader.tsx
@@ -0,0 +1,37 @@
+import classes from "./TabHeader.module.scss";
+
+interface Props {
+  tabs: Array<{
+    id: string;
+    name: string;
+  }>;
+  activeTab: string;
+  className?: string;
+  onRequestChangeTab?: (tabId: string) => void;
+}
+
+const TabHeader = ({
+  tabs,
+  activeTab,
+  className = "",
+  onRequestChangeTab,
+}: Props) => {
+  return (
+    <ul className={`${classes.root} ${className}`}>
+      {tabs.map((tab, i) => (
+        <li
+          key={`tab-${i}`}
+          className={`${classes.tab} ${
+            tab.id === activeTab ? classes.active : ""
+          }`}
+        >
+          <button onClick={() => onRequestChangeTab?.(tab.id)}>
+            {tab.name}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TabHeader;
