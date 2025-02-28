86bb6640637d38f5e19d12c1e7b96fb24ca3ba7a
James Olinya <olinyajames@gmail.com>
2025-02-28 22:27:40 +0000

    implement main layout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,19 +1,21 @@
-import { ReactNode, useState } from 'react';
-import { Link } from 'react-router-dom';
-import Sidebar from './Sidebar';
-import TopNav from './TopNav';
+import { ReactNode, useState } from "react";
+import { Link } from "react-router-dom";
+import Sidebar from "./Sidebar/Sidebar";
+import TopNav from "./TopNav";
 
 interface Props {
   children: ReactNode;
 }
 
 const MainLayout = ({ children }: Props) => {
+  const [show, setShow] = useState(false);
+
   return (
-    <div className='flex'>
-      <Sidebar />
-      <div className="flex-grow ml-0 md:ml-1/3">
-        <TopNav />
-        {children}
+    <div className="flex min-h-screen bg-[#f5f7fa]">
+      <Sidebar show={show} onRequestClose={() => setShow(false)} />
+      <div className="flex-1 flex flex-col">
+        <TopNav onRequestToggle={() => setShow((val) => !val)} />
+        <div className="flex-1">{children}</div>
       </div>
     </div>
   );
