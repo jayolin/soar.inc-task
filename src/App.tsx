6f7ecfb7f1505b923b1db33ae064d5c3a4634974
James Olinya <olinyajames@gmail.com>
2025-02-28 23:06:21 +0000

    lazy load pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AppProvider } from './context/AppContext';
-import Dashboard from './pages/Dashboard';
-import Settings from './pages/Settings';
 import MainLayout from './components/layout/MainLayout';
+
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const Settings = lazy(() => import('./pages/Settings'));
 
 const App = () => {
   return (
     <AppProvider>
       <Router>
         <MainLayout>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/settings" element={<Settings />} />
-          </Routes>
+          <Suspense  fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/settings" element={<Settings />} />
+            </Routes>
+          </Suspense>
         </MainLayout>
       </Router>
     </AppProvider>
